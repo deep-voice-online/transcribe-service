d58244b45cfdad8d6223cbfd2f46b150ed3f4137
sergey <wendingnn@gmail.com>
2026-03-02 16:45:03 +0300

    chore: enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,19 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: true, // Разрешить все источники (можно указать конкретные домены)
+    credentials: true, // Разрешить отправку cookies
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+  });
+
   const config = app.get(ConfigService);
 
-  const port = config.getOrThrow<number>('HTTP_PORT')
+  const port = config.getOrThrow<number>('HTTP_PORT');
 
   await app.listen(port);
   console.log(`Running on ${port}`);
 }
+
 bootstrap();
